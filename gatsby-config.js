9070e0b67c8f1fc015db71bdd83e44f0a44abc68
faeterain <tanphat.uit@gmail.com>
2021-11-07 22:07:22 +0700

    solve conflict

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,26 @@
-<<<<<<< HEAD
-
 module.exports = {
+  siteMetadata: {
+    title: `Taxbook`,
+    description: `Taxbook - Dịch vụ Kế toán & Thuế`,
+    author: `Phat Nguyen`,
+    menuLinks:[
+        {
+          name: `Trang chủ`,
+          link: `/`
+        },
+        {
+          name: `Sản phẩm`,
+          link: `/Product`
+        },
+        {
+          name: `Blog`,
+          link: `/Blog`
+        },
+        {
+          name: `Chuyên gia`,
+          link: `/Prof`
+        }
+    ]
+  },
+  plugins: [`gatsby-plugin-sass`]
 }
-=======
-module.exports = {
-    siteMetadata: {
-      title: `Taxbook`,
-      description: `Taxbook - Dịch vụ Kế toán & Thuế`,
-      author: `Phat Nguyen`,
-      menuLinks:[
-          {
-						name: `Trang chủ`,
-						link: `/`
-          },
-          {
-            name: `Sản phẩm`,
-            link: `/Product`
-        	},
-          {
-            name: `Blog`,
-            link: `/Blog`
-        	},
-          {
-            name: `Chuyên gia`,
-            link: `/Prof`
-        	}
-      ]
-    },
-    plugins: [`gatsby-plugin-sass`]
-  }
->>>>>>> 19b22ef328525b55670769cb23fffe0e8580833e
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import * as React from 'react'
-import { Link } from 'gatsby'
-
-export default function Home (props) {
-  return (
-    <>
-      <h1>Rendering Modes Starter</h1>
-      <ul>
-        <li>
-          <Link to='/dsg'>DSG page</Link>
-        </li>
-        <li>
-          <Link to='/ssr'>SSR page</Link>
-        </li>
-      </ul>
-=======
 import React, { useEffect } from "react"
 import AOS from "aos"
 import './index.scss'
@@ -31,7 +14,6 @@ export default function Home (props) {
       <HomeLayout title="Home">
         <h2>Home page</h2>
       </HomeLayout>
->>>>>>> 19b22ef328525b55670769cb23fffe0e8580833e
     </>
   )
 }
